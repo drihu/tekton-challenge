ac7d2c7dd6c244cec5ab2a79a7fe462a2e71dcbc
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-08-06 16:32:39 -0500

    feat(api): disable CORS

--- api-backend/app/controllers/application_controller.rb
+++ api-backend/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  before_action :access_any_origin
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: { message: e.message }, status: :not_found
+  end
+
+  private
+  def access_any_origin
+    headers['Access-Control-Allow-Origin'] = '*'
+    return
+  end
 end
