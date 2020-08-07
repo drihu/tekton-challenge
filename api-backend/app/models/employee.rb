71e504ce4ae0eee202eecf80b8704fdc306dc382
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-08-06 20:27:16 -0500

    feat(api): add photo_url to Employees

--- api-backend/app/models/employee.rb
+++ api-backend/app/models/employee.rb
@@ -1,4 +1,6 @@
 class Employee < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :position, presence: true
@@ -8,4 +10,16 @@ class Employee < ApplicationRecord
   enum gender: { male: 0, female: 1 }
 
   has_one_attached :avatar
+
+  after_save :add_photo_url, if: :not_photo_url?
+
+  private
+  def add_photo_url
+    photo_url = url_for(self.avatar)
+    self.update(photo_url: photo_url)
+  end
+
+  def not_photo_url?
+    self.photo_url == nil && self.avatar && self.avatar.attached?
+  end
 end
--- api-backend/db/schema.rb
+++ api-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_015807) do
+ActiveRecord::Schema.define(version: 2020_08_06_234919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_08_06_015807) do
     t.date "birthday"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "photo_url"
   end
 
   create_table "transactions", force: :cascade do |t|
